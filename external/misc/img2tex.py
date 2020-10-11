2b0fda058f300d4b017d326f00243d2d667c81f6
kipcode66 <egamer1010@gmail.com>
2020-10-11 18:22:34 -0400

    Added an option to resize the picture

--- external/misc/img2tex.py
+++ external/misc/img2tex.py
@@ -1,7 +1,19 @@
 from PIL import Image
 import struct
 import argparse
-import sys
+import re
+
+def size_type(string):
+    pattern = re.compile("([0-9]+)[xX]([0-9]+)")
+    matching = re.fullmatch(pattern, string)
+    if matching is None:
+        msg = "%r is not a valid size" % string
+        raise argparse.ArgumentTypeError(msg)
+    size = tuple(int(g) for g in matching.groups())
+    if size[0] % 4 != 0 or size[1] % 4 != 0:
+        msg = "The new size \"%dx%d\" must be a multiple of 4" % size
+        raise argparse.ArgumentTypeError(msg)
+    return size
 
 def write_header(out_file, image):
     # If the picture doesn't have opacity, we add it
@@ -31,15 +43,19 @@ def write_tile(out_file, image, tile_x, tile_y, pair):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Converts an image into a texture file.')
-    parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.1')
-    parser.add_argument('image_file', type=str,
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.2')
+    parser.add_argument('image_file', type=argparse.FileType('rb'),
                         help='Path to the image file')
     parser.add_argument('out_tex', type=argparse.FileType('wb'), nargs='?', default="out.tex",
                         help='Name of the output file')
+    parser.add_argument('-r', '--resize', type=size_type, metavar='WxH',
+                        help='Resizes the image to a width/height of W/H')
     params = parser.parse_args()
 
     # We fetch the picture, and make sure that it has a valid size
     image = Image.open(params.image_file)
+    if not params.resize is None:
+        image = image.resize(params.resize)
     if image.width % 4 != 0 or image.height % 4 != 0:
         raise RuntimeError("image size has to be a multiple of 4")
 
